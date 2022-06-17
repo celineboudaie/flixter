9b86f8fabcdf037023df523ab73c11b4cdb49437
celineboudaie <41020735+celineboudaie@users.noreply.github.com>
2022-06-16 17:55:50 -0700

    API implementation + cells

---
+++ Flixter/MovieViewController.m
@@ -0,0 +1,64 @@
+//
+//  MovieViewController.m
+//  Flixter
+//
+//  Created by Celine Boudaie on 6/15/22.
+//
+
+#import "MovieViewController.h"
+
+@interface MovieViewController ()<UITableViewDataSource, UITableViewDelegate>
+
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
+@property (nonatomic, strong) NSArray *resultsArray;
+@end
+
+@implementation MovieViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.tableView.dataSource = self;
+    self.tableView.delegate = self;
+    NSURL *url = [NSURL URLWithString:@"https://api.themoviedb.org/3/movie/now_playing?api_key=7f1849e764fef1875bf464a4720a3799"];
+    NSURLRequest *request = [NSURLRequest requestWithURL:url cachePolicy:NSURLRequestReloadIgnoringLocalCacheData timeoutInterval:10.0];
+    NSURLSession *session = [NSURLSession sessionWithConfiguration:[NSURLSessionConfiguration defaultSessionConfiguration] delegate:nil delegateQueue:[NSOperationQueue mainQueue]];
+    NSURLSessionDataTask *task = [session dataTaskWithRequest:request completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+           if (error != nil) {
+               NSLog(@"%@", [error localizedDescription]);
+           }
+           else {
+               NSDictionary *dataDictionary = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingMutableContainers error:nil];
+
+               NSArray *resultsArray = dataDictionary[@"results"];
+               [self.tableView reloadData];
+               // TODO: Store the movies in a property to use elsewhere
+               // TODO: Reload your table view data
+           }
+       }];
+    
+    [task resume];
+    // Do any additional setup after loading the view.
+}
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    UITableViewCell *cell = [[UITableViewCell alloc] init];
+   
+    NSDictionary *movie = self.resultsArray[indexPath.row];
+    cell.textLabel.text = movie[@"title"];
+    
+    
+    return  cell;
+}
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    return self.resultsArray.count;
+}
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
---
+++ Flixter/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  Flixter
+//
+//  Created by Celine Boudaie on 6/15/22.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
